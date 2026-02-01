24dda4d02fc7c8e55ce5ecb0fb0c19b68059c964
Atyson Jaime <atysonjaime@gmail.com>
2026-02-01 19:46:42 -0300

    feat: Integrate Aside componente

--- src/components/Layout/Aside/index.tsx
+++ src/components/Layout/Aside/index.tsx
@@ -1,30 +1,50 @@
-import { Building, Instagram, Link2, MapPin, Smile, User } from "lucide-react"
+import { TUserGithub } from "@/interfaces/user.interface"
+import { Building, Link2, Mail, MapPin, User } from "lucide-react"
+import Image from "next/image"
 import Link from "next/link"
 import { Accordion } from "react-bootstrap"
+import { BsTwitter } from "react-icons/bs"
 
-export default function Aside() {
+type TAsideProps = {
+	user: TUserGithub
+}
+
+export default function Aside({ user }: TAsideProps) {
 	return (
 		<aside className="flex w-full flex-col gap-11 sm:max-w-[217px]! lg:max-w-[255px]!">
 			<div className="flex flex-col items-center justify-center gap-6">
 				{/* Foto do usuário */}
 				<div className="relative flex h-[104px]! w-[104px] items-center justify-center lg:h-[150px]! lg:w-[150px]">
-					<div className="flex h-full w-full items-center justify-center rounded-full! bg-(--gray-bg)! shadow-(--box-shadow-inner)!">
-						<User size={50} className="text-(--gray-3)!" />
+					<div className="relative flex h-full w-full items-center justify-center rounded-full! bg-(--gray-bg)! shadow-(--box-shadow-inner)!">
+						<div className="absolute right-0 bottom-0 flex h-[104px]! w-[104px] items-center justify-center rounded-full! shadow-(--box-shadow-inner)! lg:h-[150px]! lg:w-[150px]"></div>
+						{user.avatar_url ? (
+							<Image
+								src={user.avatar_url}
+								alt={user.login}
+								width={104}
+								height={104}
+								className="h-full! w-full! rounded-full! object-cover! shadow-(--box-shadow-inner)"
+							/>
+						) : (
+							<User size={50} className="text-(--gray-3)!" />
+						)}
 					</div>
 					<div className="absolute right-0 bottom-0 flex h-[28px] w-[28px] items-center justify-center rounded-full! bg-white shadow-(--box-shadow)! lg:h-[40px] lg:w-[40px]">
-						<Smile size={20} className="text-(--gray-3)!" />
+						😎
 					</div>
 				</div>
 				{/* Info do usuário */}
 				<div className="flex w-full max-w-[217px]! flex-col items-center justify-center gap-1 sm:max-w-full!">
-					<h5 className="text-[20px]! font-bold! lg:text-[24px]!">Teste</h5>
+					<h5 className="text-[20px]! font-bold! lg:text-[24px]!">
+						{user.name}
+					</h5>
 					<p className="text-center! text-[12px]! text-(--gray-3)! md:text-[14px]! lg:text-[16px]!">
-						Head development team Front-End Magazord - Tagged (#BZ)
+						{user.bio}
 					</p>
 				</div>
 			</div>
 			<div className="hidden sm:block!">
-				<ListLinks />
+				<ListLinks user={user} />
 			</div>
 			<Accordion className="block sm:hidden!">
 				<Accordion.Item eventKey="0" className="rounded-none! border-0!">
@@ -32,7 +52,7 @@ export default function Aside() {
 						<p className="text-(--primary)!">Informações Adicionais</p>
 					</Accordion.Header>
 					<Accordion.Body className="mt-2! rounded-2xl! bg-(--gray-bg)! p-4!">
-						<ListLinks />
+						<ListLinks user={user} />
 					</Accordion.Body>
 				</Accordion.Item>
 			</Accordion>
@@ -40,35 +60,71 @@ export default function Aside() {
 	)
 }
 
-function ListLinks() {
+type TListLinksProps = {
+	user: TUserGithub
+}
+function ListLinks({ user }: TListLinksProps) {
 	const linkClassName =
 		"flex items-center gap-2.5 text-(--primary)! hover:text-(--primary-dark)! hover:underline! focus:text-(--primary-dark)! focus:underline! active:text-(--primary-dark)! active:underline! transition-all duration-300!"
 	return (
 		<ul className="m-0! flex list-none! flex-col gap-4 p-0!">
 			<li>
-				<Link href={"/"} className={linkClassName}>
+				<Link
+					href={user.html_url ? user.html_url : "#"}
+					className={linkClassName}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
 					<Building size={20} />
-					Megazord - plataforma
+					{user.company || ""}
 				</Link>
 			</li>
 			<li>
-				<Link href={"/"} className={linkClassName}>
+				<Link
+					href={user.html_url ? user.html_url : "#"}
+					className={linkClassName}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
 					<MapPin size={20} />
-					São Paulo, SP
+					{user.location || ""}
 				</Link>
 			</li>
 			<li>
-				<Link href={"/"} className={linkClassName}>
+				<Link
+					href={user.blog || "#"}
+					className={linkClassName}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
 					<Link2 size={20} />
-					Cordas.hub.uok
+					{user.blog || ""}
 				</Link>
 			</li>
 			<li>
-				<Link href={"/"} className={linkClassName}>
-					<Instagram size={20} />
-					@atysonjaime
+				<Link
+					href={"mailto:" + user.email || "#"}
+					className={linkClassName}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<Mail size={20} />
+					{user.email || ""}
 				</Link>
 			</li>
+			{user.twitter_username && (
+				<li>
+					<Link
+						href={user.html_url || "#"}
+						className={linkClassName}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<BsTwitter size={20} />
+						{user.twitter_username || ""}
+					</Link>
+				</li>
+			)}
 		</ul>
 	)
 }
