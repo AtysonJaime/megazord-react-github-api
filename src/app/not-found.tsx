10c669249b1e17a6a6f9563a2416772ab70577cd
Atyson Jaime <atysonjaime@gmail.com>
2026-01-31 23:12:47 -0300

    feat: Add not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,22 @@
+import Link from "next/link"
+
 export default function NotFound() {
-	// Redireciona automaticamente para a página customizada de not-found
-	// TODO: Ajustar página
 	return (
-		<div>
-			<h1>404 - Página não encontrada</h1>
+		<div className="flex h-full w-full flex-col items-center justify-center gap-3">
+			<h2 className="z-1 m-0 text-center">Rota acessada não existe</h2>
+			<h4 className="z-1 m-0 max-w-300 text-center">
+				Não conseguimos encontrar a rota desejada, por favor, verifique o
+				endereço informado.
+			</h4>
+			<div className="z-1 flex flex-wrap items-center justify-center gap-2">
+				<Link
+					href={"/"}
+					className="rounded-md! border border-(--primary)! bg-transparent! p-2! font-bold! text-(--primary)! transition-all duration-300! hover:bg-(--primary)! hover:text-white!"
+					type="button"
+				>
+					Voltar para a página inicial
+				</Link>
+			</div>
 		</div>
 	)
 }
